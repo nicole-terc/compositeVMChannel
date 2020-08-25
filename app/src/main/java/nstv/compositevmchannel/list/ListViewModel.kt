06c8bc31d8000b7e828d62dee62fdfb275a9d419
Nicole Terc <nicole.terc@mobimeo.com>
2020-08-25 20:07:07 +0200

    add sample UDF ListViewModel

--- app/src/main/java/nstv/compositevmchannel/list/ListViewModel.kt
+++ app/src/main/java/nstv/compositevmchannel/list/ListViewModel.kt
@@ -1,7 +1,35 @@
 package nstv.compositevmchannel.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import nstv.compositevmchannel.data.model.Event
+import nstv.compositevmchannel.data.model.State
+import nstv.compositevmchannel.useCase.LoadItemsUseCase
 
-class ListViewModel : ViewModel() {
+class ListViewModel(
+    val loadItemsUseCase: LoadItemsUseCase
+) : ViewModel() {
+    val state: Channel<State> = Channel()
 
+    fun send(event: Event) {
+        when (event) {
+            is Event.Load -> loadData()
+        }
+    }
+
+    private fun loadData() {
+        viewModelScope.launch {
+            state.offer(State.Loading)
+
+            val items = loadItemsUseCase()
+
+            if (items.isEmpty()) {
+                state.offer(State.Error("Error fetching items"))
+            } else {
+                state.offer(State.ShowData(items))
+            }
+        }
+    }
 }
